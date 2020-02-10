1fb6d386e7c38a5df5f2e0beb106a48e5c3dc9f9
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-02-10 12:00:13 -0600

    works

--- exception_utils.py
+++ exception_utils.py
@@ -1,6 +1,6 @@
 
 
-# test comment
+
 def error_if_param_invalid(param, valid_param_l, custom_msg = None):
     if custom_msg == None:
         msg = "ERROR:  Invalid Param.:  " + str(param) + ", must be one of: " + str(valid_param_l)
