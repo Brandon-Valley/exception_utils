1e84ad14c4390a6fd80424dc028aa36431ece040
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-02-11 15:03:29 -0600

    added ability to relative import

--- exception_utils.py
+++ exception_utils.py
@@ -1,3 +1,9 @@
+''' [======- - - - -=================- All Utilities Standard -=================- - - - -======] '''
+# to allow for relative imports
+import sys, os
+sys.path.insert(1, os.path.join(sys.path[0], os.path.dirname(os.path.abspath(__file__))))
+''' [======- - - - - - -=============- - - - -========- - - - -=============- - - - - - -======] '''
+
 
 
 # test commit 0
