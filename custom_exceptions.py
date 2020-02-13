0e66256fed08ce568af058489cb99ca4e41d0e42
Valley (US), Brandon A <brandon.a.valley@boeing.com>
2020-02-13 13:00:34 -0600

    added submodule management to allow for nested submodule imports

--- custom_exceptions.py
+++ custom_exceptions.py
@@ -2,5 +2,7 @@
 
 # use when you have a function that has a set of incompatible 
 # parameter options you would like to protect against
-class ForbiddenParamValComboError(Exception):
-    pass
+class ForbiddenParamValComboError(Exception): pass
+
+
+class ParamKeyNotInWhitelistError(Exception): pass
